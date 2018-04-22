7ba3ea4d308348b13460cafd612d27bacee7edf4
Valtteri Kortteisto <valtteri.kortteisto@aalto.fi>
2018-04-22 16:25:46 +0300

    intro menu buttons now react to mouse

--- TD1/src/user_interface/IntroMenu.scala
+++ TD1/src/user_interface/IntroMenu.scala
@@ -15,36 +15,47 @@ class IntroMenu(g: Game, p: Progress) extends Helper(g) {
     ???
   }
   
-  def onEasy = ???
+  def onEasy = (mouseX > 6 * sqSize) && (mouseY > 3* sqSize) &&
+               (mouseX < 6 * sqSize + buttonSize._1 * sqSize) && 
+               (mouseY < 3 * sqSize + buttonSize._2 * sqSize)
+               
   def onNormal = ???
   def onHard = ???
   def onInsane = ???
   
   def onCredits = ???
   
-  def doStuff() = {
-    g.image(g.introImg(0), 0, 0, aWidth*sqSize+mWidth*sqSize, aHeight*sqSize)
+  val buttonSize = (16 * sqSize,3 * sqSize)
+  
+  /**All the buttons in this class follow the same format. */
+  def button(text: String, pos: Int) = {
+    def onButton = (mouseX > 6 * sqSize) && (mouseY > pos * sqSize) &&
+                   (mouseX < 6 * sqSize + buttonSize._1) && 
+                   (mouseY < pos * sqSize + buttonSize._2)
+    if (onButton) {
+      //g.stroke(0)
+      g.fill(255,255,255,150)
+      g.rect(6*sqSize,pos*sqSize,buttonSize._1,buttonSize._2)
+    } else {
+      //g.noStroke()
+      g.fill(100,100,100,100)
+      g.rect(6*sqSize,pos*sqSize,buttonSize._1,buttonSize._2)
+    }
+    g.textFont(g.font,24)
+    g.fill(0)
+    g.text(text,6*sqSize + (buttonSize._1/2),pos*sqSize + (buttonSize._2/2))
+  } // TODO: Really slow for some reason?
+  
+  def doStuff() = { // processing doesnt like drawing big images // TODO: arena in the background.
+    //g.image(g.introImg(0), 0, 0, aWidth*sqSize+mWidth*sqSize, aHeight*sqSize)
     g.textFont(g.font,24)
     g.noStroke()
     g.fill(0)
     g.text("Desert Tower Defence",11.4.toFloat*sqSize,1.65.toFloat*sqSize)
-    g.fill(100,100,100,100)
-    g.rect(6*sqSize,3*sqSize,16*sqSize,3*sqSize)
-    g.fill(0)
-    g.text("Easy Levels",11.4.toFloat*sqSize,4.65.toFloat*sqSize)
-    g.fill(100,100,100,100)
-    g.rect(6*sqSize,7*sqSize,16*sqSize,3*sqSize)
-    g.fill(0)
-    g.text("Normal Levels",11.4.toFloat*sqSize,8.65.toFloat*sqSize)
-    g.fill(100,100,100,100)
-    g.rect(6*sqSize,11*sqSize,16*sqSize,3*sqSize)
-    g.fill(0)
-    g.text("Hard Levels",11.4.toFloat*sqSize,12.65.toFloat*sqSize)
-    g.fill(100,100,100,100)
-    g.rect(6*sqSize,15*sqSize,16*sqSize,3*sqSize)
-    g.fill(0)
-    g.text("Credits",11.4.toFloat*sqSize,16.65.toFloat*sqSize)
-    g.fill(100,100,100,100)
+    button("Easy Levels",3)
+    button("Normal Levels",7)
+    button("Hard Levels",11)
+    button("Credits",15)
   }
   
 }
